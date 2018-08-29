0eb42ec1264887108a361a41e2191e79d7482d45
Will <w.kopp@kigroup.de>
2018-08-29 16:09:55 +0200

    Add some doc

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:carbon
+FROM node:10-alpine
 
 WORKDIR /usr/src/app
 
---
+++ doc/plugin.js
@@ -0,0 +1,25 @@
+import Plugin from '../../lib/lib'
+import pluginConfig from './config'
+
+module.exports = class SuperPlugin extends Plugin {
+  constructor (...args) {
+    super(args)
+    this.config[this.name] = pluginConfig
+  }
+
+  start () {
+    super.start()
+
+    // Interact with Polkadot
+    this.polkadot.chain
+      .newHead((error, header) => {
+        // ...
+      })
+      .catch(e => console.log)
+
+    // Interact with Matrix.org
+    this.matrix.on('Room.timeline', (event, room, toStartOfTimeline) => {
+      // ...
+    })
+  }
+}
