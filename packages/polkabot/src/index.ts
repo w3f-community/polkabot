fa1cb3e662f79015527356c8fb2958d07629f0b5
Will <w.kopp@kigroup.de>
2020-01-21 17:14:57 +0100

    upgrade dependencies

--- packages/polkabot/src/index.ts
+++ packages/polkabot/src/index.ts
@@ -1,21 +1,21 @@
 // import 'babel-core/register'
 // import 'babel-polyfill'
-import Olm from 'olm'
-import minimongo from 'minimongo'
-import { ApiPromise, WsProvider } from '@polkadot/api'
-import Datastore from 'nedb'
+import Olm from "olm";
+import minimongo from "minimongo";
+import { ApiPromise, WsProvider } from "@polkadot/api";
+import Datastore from "nedb";
 
-import pkg from '../package.json'
-import PluginScanner from './lib/plugin-scanner'
-import PluginLoader from './lib/plugin-loader'
+import pkg from "../package.json";
+import PluginScanner from "./lib/plugin-scanner";
+import PluginLoader from "./lib/plugin-loader";
 // import * as path from 'path'
-import sdk from 'matrix-js-sdk'
-import { ConfigSingleton } from './ConfigSingleton';
-import { assert } from '@polkadot/util';
-import { IPolkabotConfig } from './types';
+import sdk from "matrix-js-sdk";
+import { ConfigSingleton } from "./ConfigSingleton";
+import { assert } from "@polkadot/util";
+import { IPolkabotConfig } from "./types";
 
 //@ts-ignore
-global.Olm = Olm
+global.Olm = Olm;
 
 // comment out if you need to trouble shoot matrix issues
 // matrix.on('event', function (event) {
@@ -29,37 +29,41 @@ export default class Polkabot {
   private matrix: any;
   private polkadot: any;
 
-  public constructor (args) {
+  public constructor(args) {
     // this.args = args
-    this.db = new Datastore({ filename: 'polkabot.db' })
+    this.db = new Datastore({ filename: "polkabot.db" });
   }
 
-  private loadPlugins (): void {
-    console.log('Polkabot - Loading plugins:')
-    const pluginScanner = new PluginScanner(pkg.name + '-plugin')
+  private loadPlugins(): void {
+    console.log("Polkabot - Loading plugins:");
+    const pluginScanner = new PluginScanner(pkg.name + "-plugin");
 
-    pluginScanner.scan((err, module) => {
-      if (err) console.error(err)
-      const pluginLoader = new PluginLoader(module)
-      pluginLoader.load(Plugin => {
-        const plugin = new Plugin({
-          config: this.config,
-          pkg: pkg,
-          db: this.db,
-          matrix: this.matrix,
-          polkadot: this.polkadot })
-        plugin.start()
-      })
-    }, (err, all) => {
-      if (err) console.error(err)
-      console.log()
-      if (all.length === 0) {
-        console.log('Polkabot - Polkabot does not do much without plugin, make sure you install at least one')
+    pluginScanner.scan(
+      (err, module) => {
+        if (err) console.error(err);
+        const pluginLoader = new PluginLoader(module);
+        pluginLoader.load(Plugin => {
+          const plugin = new Plugin({
+            config: this.config,
+            pkg: pkg,
+            db: this.db,
+            matrix: this.matrix,
+            polkadot: this.polkadot
+          });
+          plugin.start();
+        });
+      },
+      (err, all) => {
+        if (err) console.error(err);
+        console.log();
+        if (all.length === 0) {
+          console.log("Polkabot - Polkabot does not do much without plugin, make sure you install at least one");
+        }
       }
-    })
+    );
   }
 
-  private start (_syncState): void {
+  private start(_syncState): void {
     // Send message to the room notifying users of the bot's state
 
     // we dont want to bother users, the following should be removed
@@ -85,12 +89,12 @@ export default class Polkabot {
     //     }
     //   )
 
-    this.loadPlugins()
+    this.loadPlugins();
   }
 
-  public async run () {
-    console.log(`${pkg.name} v${pkg.version}`)
-    console.log('===========================')
+  public async run() {
+    console.log(`${pkg.name} v${pkg.version}`);
+    console.log("===========================");
 
     // const configLocation = this.args.config
     //   ? this.args.config
@@ -98,77 +102,79 @@ export default class Polkabot {
     // console.log('Polkabot - Config location: ', configLocation)
 
     // this.config = require(configLocation)
-    
-    let config: IPolkabotConfig = ConfigSingleton.getInstance()
-    assert(config.polkadot.host != null, 'Issue with the config')
-    assert(config.matrix.botMasterId != null, 'Missing bot master id')
-    ConfigSingleton.dumpEnv()
-    
-    this.config = config
-    
-    // console.log(`Polkabot - config: ${JSON.stringify(this.config, null, 2)}`)
-    // console.log(`Polkabot - Connecting to host: ${this.config.polkadot.host}`)
-    // console.log(`Polkabot - Running with bot user id: ${this.config.matrix.botUserId}`)
 
-    const provider = new WsProvider(this.config.polkadot.host)
+    let config: IPolkabotConfig = ConfigSingleton.getInstance();
+    assert(config.polkadot.host != null, "Issue with the config");
+    assert(config.matrix.botMasterId != null, "Missing bot master id");
+    ConfigSingleton.dumpEnv();
+
+    this.config = config;
+
+    console.log(`Polkabot - config: ${JSON.stringify(this.config, null, 2)}`);
+    console.log(`Polkabot - Connecting to host: ${this.config.polkadot.host}`);
+    console.log(`Polkabot - Running with bot user id: ${this.config.matrix.botUserId}`);
+
+    const provider = new WsProvider(this.config.polkadot.host);
     // Create the API and wait until ready
-    this.polkadot = await ApiPromise.create({provider})
+    console.log("Create prov");
+    this.polkadot = await ApiPromise.create({ provider });
+
+    console.log("get chain data");
 
     // Retrieve the chain & node information information via rpc calls
     const [chain, nodeName, nodeVersion] = await Promise.all([
       this.polkadot.rpc.system.chain(),
       this.polkadot.rpc.system.name(),
       this.polkadot.rpc.system.version()
-    ])
+    ]);
 
-    console.log(`Polkabot - You are connected to chain ${chain} using ${nodeName} v${nodeVersion}`)
+    console.log(`Polkabot - You are connected to chain ${chain} using ${nodeName} v${nodeVersion}`);
 
-    const LocalDb = minimongo.MemoryDb
-    this.db = new LocalDb()
-    this.db.addCollection('config')
+    const LocalDb = minimongo.MemoryDb;
+    this.db = new LocalDb();
+    this.db.addCollection("config");
 
     this.db.config.upsert({ botMasterId: this.config.matrix.botMasterId }, () => {
       this.db.config.findOne({}, {}, res => {
-        console.log('Polkabot - Matrix client bot manager id: ' + res.botMasterId)
-      })
-    })
+        console.log("Polkabot - Matrix client bot manager id: " + res.botMasterId);
+      });
+    });
 
     // TODO - refactor using async/await. See https://github.com/matrix-org/matrix-js-sdk/issues/789
-       console.log('Polkabot - creating client')
-    
+    console.log("Polkabot - creating client");
+
     this.matrix = sdk.createClient({
       baseUrl: this.config.matrix.baseUrl,
       accessToken: this.config.matrix.token,
       userId: this.config.matrix.botUserId
-    })
+    });
 
     if (this.isCustomBaseUrl()) {
-      const data = await this.matrix.login(
-        'm.login.password',
-        {
+      const data = await this.matrix
+        .login("m.login.password", {
           user: this.config.matrix.loginUserId,
           password: this.config.matrix.loginUserPassword
-        }
-      ).catch(error => {
-        console.error('Polkabot: Error logging into matrix:', error)
-      })
+        })
+        .catch(error => {
+          console.error("Polkabot: Error logging into matrix:", error);
+        });
 
       if (data) {
-        console.log('Polkabot - Logged in with credentials: ', data)
+        console.log("Polkabot - Logged in with credentials: ", data);
       }
     }
 
-    this.matrix.once('sync', (state, _prevState, data) => {
+    this.matrix.once("sync", (state, _prevState, data) => {
       switch (state) {
-        case 'PREPARED':
-          console.log(`Polkabot - Detected client sync state: ${state}`)
-          this.start(state)
-          break
+        case "PREPARED":
+          console.log(`Polkabot - Detected client sync state: ${state}`);
+          this.start(state);
+          break;
         default:
-          console.log('Polkabot - Error. Unable to establish client sync state, state =', state, data)
-          process.exit(1)
+          console.log("Polkabot - Error. Unable to establish client sync state, state =", state, data);
+          process.exit(1);
       }
-    })
+    });
 
     // // Event emitted when member's membership changes
     // this.matrix.on('RoomMember.membership', (event, member) => {
@@ -189,12 +195,12 @@ export default class Polkabot {
     //   }
     // })
 
-    this.matrix.startClient(this.config.matrix.MESSAGES_TO_SHOW || 20)
+    this.matrix.startClient(this.config.matrix.MESSAGES_TO_SHOW || 20);
   }
 
-  private isCustomBaseUrl () {
-    const { baseUrl } = this.config.matrix
+  private isCustomBaseUrl() {
+    const { baseUrl } = this.config.matrix;
 
-    return baseUrl && baseUrl !== 'https://matrix.org'
+    return baseUrl && baseUrl !== "https://matrix.org";
   }
 }
