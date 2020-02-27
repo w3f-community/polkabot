37f1909167c811342ef734cfa88df39350614ad3
Will <w.kopp@kigroup.de>
2020-02-27 10:21:16 +0100

    Package cleanup

--- packages/config-singleton/test/config-specs.ts
+++ packages/config-singleton/test/config-specs.ts
@@ -1,25 +1,14 @@
-import { ConfigSpecs } from '../src/types';
 import { SpecsFactory } from '../src/SpecsFactory';
 
 const prefix = 'SAMPLE';
 const mod = 'MODULE';
-// let specs: ConfigSpecs = {
-//   container: {
-//     prefix,
-//     module: mod,
-//   },
-//   config: {},
-// };
-export let param;
 
 const factory = new SpecsFactory({ prefix, module: mod });
-
 factory.appendSpec(factory.getSpec('PARAM1', 'some param1'));
 factory.appendSpec(factory.getSpec('PARAM2', 'some param2'));
 factory.appendSpec(factory.getSpec('SECRET', 'some secret', { masked: true }));
 factory.appendSpec(factory.getSpec('REGEXP', 'some regexp', { regexp: /^\d{2}_\d{2}/ }));
 factory.appendSpec(factory.getSpec('MANDAT1', 'some mandatory param', { mandatory: true }));
-// specs = factory.appendSpec(specs, factory.getSpec('REGEXP', 'some regexp', /^\d{2}_\d{2}/));
 const specs = factory.getSpecs()
 
 export default specs;
